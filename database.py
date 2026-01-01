d7d22a1ec8bdd9d6b07f35df756b447b60595908
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-01-01 15:04:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
 from config import MONGO_URI
 
+# Mongo connection
 mongo = MongoClient(MONGO_URI)
 db = mongo.forcejoinbot
 
+# Collections
 users = db.users
+channels = db.channels     # ✅ FORCE JOIN CHANNELS
+banned = db.banned         # (future use)
+premium = db.premium       # (future use)
