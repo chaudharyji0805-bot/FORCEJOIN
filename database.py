c19f57fa048483f5aaf14bccec332b9274b1bc55
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-01-01 16:15:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,16 @@ from config import MONGO_URI
 mongo = MongoClient(MONGO_URI)
 db = mongo.forcejoinbot
 
-# collections
-users = db.users                 # all users (private + group)
-group_channels = db.group_channels  # per-group force join
+users = db.users
+
+group_settings = db.group_settings
+# example document:
+# {
+#   group_id: -100123,
+#   enabled: true,
+#   admins: [12345, 67890],
+#   channels: [
+#       {username: "ch1", invite: "..."},
+#       {username: "ch2", invite: None}
+#   ]
+# }
